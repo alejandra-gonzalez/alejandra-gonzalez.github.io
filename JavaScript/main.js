36ccd64f8fcf86df48c034c56fa426383d8eda1f
alejandra-gonzalez <gonzalez.alejandra.013@gmail.com>
2018-10-23 23:58:19 -0700

    sidebar update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,23 +1,6 @@
-var btn = document.getElementsByClassName("menu-button");
-var closeBtn = document.getElementsByClassName("close-menu");
-var menu = document.getElementsByClassName("sidebar");
-
-function showMenu() {
-    if (btn[0].style.display === "none") {
-        btn[0].style.display = "block";
-    } else {
-        btn[0].style.display = "none";
-        menu[0].style.display = "block";
-        closeBtn[0].style.display = "block";
-    }
+function openMenu(){
+    document.getElementById('sidebar').style.width = '250px';
 }
-
-function hideMenu() {
-    if (closeBtn[0].style.display === "none") {
-        closeBtn[0].style.display = "block";
-    } else {
-        btn[0].style.display = "block";
-        menu[0].style.display = "none";
-        closeBtn[0].style.display = "none";
-    }
+function closeMenu(){
+    document.getElementById('sidebar').style.width = '0px';
 }
