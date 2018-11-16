d39544f166e24815830261f51a76a59b8d0fbeff
alejandra-gonzalez <gonzalez.alejandra.013@gmail.com>
2018-11-15 20:47:30 -0800

    project card functionality

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,3 +4,19 @@ function openMenu(){
 function closeMenu(){
     document.getElementById('sidebar').style.width = '0px';
 }
+
+var details = document.getElementsByClassName('project-details');
+var basics = document.getElementsByClassName('project-basics');
+var img = document.getElementsByClassName('project-image');
+
+function openProjectDetails(){
+    details[0].style.display = "block";
+    basics[0].style.display = "none";
+    img[0].style.display = "none";
+}
+
+function closeProjectDetails() {
+    details[0].style.display = "none";
+    basics[0].style.display = "block";
+    img[0].style.display = "block";
+}
